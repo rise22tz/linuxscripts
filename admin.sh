01b1b00c5095bc1c70d387ab5284e2f161e97b39
rise22tz <rise22tz@student.ju.se>
2023-03-02 11:44:24 +0100

    advanced admin

--- admin.sh
+++ admin.sh
@@ -2,7 +2,23 @@
 
 set -euo pipefail
 
-echo "Enter username for the new user:"
-read user
+if [ $EUID -ne 0 ]; then
+	
+	echo "Must be run as root, or with sudo"
+	exit
+fi
 
-echo $user
+read -p "Username for new admin: " user
+
+while grep -q $user /etc/passwd; do
+	read -p "User already exists, enter another username:" user
+
+	if [ -z $user ]; then
+		echo "No input, quitting.."
+		exit
+	fi
+done
+
+echo "Creating user and adding to wheel"
+useradd -m -s /bin/bash $user
+usermod -aG wheel $user
